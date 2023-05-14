af15574305110d275c3eec20ed6b5eede38f8f48
wangyingjie <930055912@qq.com>
2023-05-14 22:45:55 +0800

    feat 关于pprof的试验

--- pprof/go-pprof-practice1/animal/felidae/felidae.go
+++
@@ -1,9 +0,0 @@
-package felidae
-
-import "github.com/wolfogre/go-pprof-practice/animal"
-
-type Felidae interface {
-	animal.Animal
-	Climb()
-	Sneak()
-}
--- pprof/go-pprof-practice1/animal/muridae/muridae.go
+++
@@ -1,9 +0,0 @@
-package muridae
-
-import "github.com/wolfogre/go-pprof-practice/animal"
-
-type Muridae interface {
-	animal.Animal
-	Hole()
-	Steal()
-}
